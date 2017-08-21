00a19794c9ffffbbc5a58bdd3d8d68d60a85e178
mrsshell <mrsshell@comcast.net>
2017-08-21 16:59:18 -0700

    starting to do psudo code

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 'use strict';
+function Analysis(custName, product,clickCount,results){
+
+this.custName = custName;
+this.product = product;
+this.clickCount = clickCount;
+this.results = 0;
+
+var imageArray = [img src="img/bag.jpg", img src="img/boots.jpg", img src="img/wine-glass.jpg", img src="img/banana.jpg", img src="img/bathroom.jpg", img src="img/breakfast.jpg", img src="img/bubblegum.jpg", img src="img/chair.jpg", img src="img/cthulhu.jpg", img src="img/dag-duck.jpg",
+img src="img/dragon.jg", img src="img/pen.jpg", img src="img/pen.jpg", img src="pet-sweep.jpg", img src="img/scissors.jpg", img src="img/shark.jpg", img src="img/sweep.jpg", img src="img/sweep.jpg", img src="img/tauntaun.jpg", img src="img/unicorn.jpg", img src="img/usb.jeg", img src="img/water-can.jpg"];
+
+var getNewImage = (){
+  for (var i = 0; i === 25; i++) {
+    replace();
+    when the image is clicked, find element by Id ,then replace it with a new image[i];
+    pull from the page , put it back in the array;
+    slide next image to the page;
+    }
+    var results = 0;
+  var countClicks = (){
+    for (var i = 0; i <= 25; i++) {
+      if the image is clicked count it[0];
+      countClicks+= results;
+    }
+};
+
+  var storeResults = (){
+    for (var i = 0; i <= 25 ; i++) {
+      store the rsults[i];
+    }
+  };
+};

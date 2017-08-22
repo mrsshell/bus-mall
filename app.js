b9c82d7794e863917c89f7d80ed38faecb018798
mrsshell <mrsshell@comcast.net>
2017-08-22 14:28:24 -0700

    addedto js

--- app.js
+++ app.js
@@ -1,64 +1,66 @@
 'use strict';
 var maxClicks = 25;
+var productArray = [];
+var upNext = [];
+place = ["firstPic","secondPic","thirdPic"];
 
-function Analysis(custName, product,clickCount,results){
-//
-
-this.custName = custName;
+function Products(name, product, place) {
+this.name = name;
 this.product = product;
-this.clickCount = clickCount;
+this.place.push(this);
+this.clickCount = 0;
 this.results = 0;
-this.id = id;
-produt.list.push(this);
+productArray.push(this);
+
 };
 //
-var productArray = ['bag',''];
-var ids = ['bag','' ];
-var upNext = ['img.bag/',];
-
-for (var i = 0; i < upNext.length; i++) {
-var product = new Product(productArray[i], upNext[i]), id [i]);
-
-};
 
 
+var bag = new Products(img src="img/bag.jpg");
+var boots = new Products(img src="img/boots.jpg");
+var  glass = new Products(img src="img/wine-glass.jpg");
+var  banana = new Products(img src="img/banana.jpg");
+bathroom = new Products(img src="img/bathroom.jpg");
+var  breakfast = new Products(img src="img/breakfast.jpg");
+var  bubblegum =  new Products(img src="img/bubblegum.jpg");
+var  chair =  new Products(img src="img/chair.jpg");
+var  cthulu = new Products(img src="img/cthulhu.jpg");
+var  dog-duck = new Products(img src="img/dog-duck.jpg");
+var  dragon = new Products(img src="img/dragon.jg");
+var  pen = new Products(img src="img/pen.jpg");
+var  pet-sweep = new Products(img src="img/pet-sweep.jpg");
+var  scissors = new Products(img src="img/scissors.jpg");
+var  shark = new Products(img src="img/shark.jpg");
+var  sweep = new Products(img src="img/sweep.jpg");
+var  tauntaun = new Products(img src="img/tauntaun.jpg");
+var  unicorn =  new Products(img src="img/unicorn.jpg");
+var  usb = new Products(img src="img/usb.jeg");
+var water-can= new Products(img src="img/water-can.jpg")
 
 
-var new bag = Bag(img src="img/bag.jpg");
-var new boots = Boots(img src="img/boots.jpg");
-var new glass = Glass(img src="img/wine-glass.jpg");
-var new banana = img src="img/banana.jpg";
-var new bathroom = img src="img/bathroom.jpg";
-var new breakfast = img src="img/breakfast.jpg";
-var new bubblegun = img src="img/bubblegum.jpg";
-var new chair =  img src="img/chair.jpg";
-var new cthulu = img src="img/cthulhu.jpg";
-var new dog-duck =  img src="img/dag-duck.jpg";
-var new dragon = img src="img/dragon.jg";
-var new pen = img src="img/pen.jpg";
-var new pet-sweep = img src="pet-sweep.jpg";
-var new scissors = img src="img/scissors.jpg";
-var new shark = img src="img/shark.jpg", img src="img/sweep.jpg", img src="img/sweep.jpg", img src="img/tauntaun.jpg", img src="img/unicorn.jpg", img src="img/usb.jeg", img src="img/water-can.jpg"];
+
+function selection() {
+var getNewImages = (){
+  for (var i = 0; i <= maxClicks.length; i++) {
+    var product = new Product(productArray[i], upNext[i]);
+
+    var upNext = function(){
+      if (mouseIsCkicked){
+      var place = document.getElementById('firstPic');
+        document.getElementById('secondPic');
+        document.getElementById('thirdPic');
+      }
+    };
+    upNext();
 
 
-function firstPic() {
-
-};
-function secondPic(){
-
-};
-
-function thirdPic(){
-
-};
-
-var getNewImage = (){
+function replace(){
   for (var i = 0; i === 25; i++) {
-    replace();
-    when the image is clicked, find element by Id ,then replace it with a new image[i];
-    pull from the page , put it back in the array;
-    slide next image to the page;
+    pull three from the page , put them back in the array;
+    slide next group of images to the page;
     }
+  };
+
     var results = 0;
   var countClicks = (){
     for (var i = 0; i <= 25; i++) {
@@ -82,7 +84,7 @@ while ( randomOne === randomTwo || nextUp.includes(nextUp));
 while (randomTwo === rand) {
 
 }
- var oneClick = document.getElementById('firstPic');
+ var oneClick = document.getElementById('firstPic','secondPic','thirdPic');
  oneClick,addEventListener('click', clickCount);
 
 

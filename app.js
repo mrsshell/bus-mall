579d78a718bf000309c7ee26bddea3780fd72d42
mrsshell <mrsshell@comcast.net>
2017-08-22 10:28:00 -0700

    worked through the list and constructed the images as objects- worked on the event listener and then the next up function

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 'use strict';
+var maxClicks = 25;
+
 function Analysis(custName, product,clickCount,results){
+//
 
 this.custName = custName;
 this.product = product;
 this.clickCount = clickCount;
 this.results = 0;
+this.id = id;
+produt.list.push(this);
+};
+//
+var productArray = ['bag',''];
+var ids = ['bag','' ];
+var upNext = ['img.bag/',];
 
-var imageArray = [img src="img/bag.jpg", img src="img/boots.jpg", img src="img/wine-glass.jpg", img src="img/banana.jpg", img src="img/bathroom.jpg", img src="img/breakfast.jpg", img src="img/bubblegum.jpg", img src="img/chair.jpg", img src="img/cthulhu.jpg", img src="img/dag-duck.jpg",
-img src="img/dragon.jg", img src="img/pen.jpg", img src="img/pen.jpg", img src="pet-sweep.jpg", img src="img/scissors.jpg", img src="img/shark.jpg", img src="img/sweep.jpg", img src="img/sweep.jpg", img src="img/tauntaun.jpg", img src="img/unicorn.jpg", img src="img/usb.jeg", img src="img/water-can.jpg"];
+for (var i = 0; i < upNext.length; i++) {
+var product = new Product(productArray[i], upNext[i]), id [i]);
+
+};
+
+
+
+
+var new bag = Bag(img src="img/bag.jpg");
+var new boots = Boots(img src="img/boots.jpg");
+var new glass = Glass(img src="img/wine-glass.jpg");
+var new banana = img src="img/banana.jpg";
+var new bathroom = img src="img/bathroom.jpg";
+var new breakfast = img src="img/breakfast.jpg";
+var new bubblegun = img src="img/bubblegum.jpg";
+var new chair =  img src="img/chair.jpg";
+var new cthulu = img src="img/cthulhu.jpg";
+var new dog-duck =  img src="img/dag-duck.jpg";
+var new dragon = img src="img/dragon.jg";
+var new pen = img src="img/pen.jpg";
+var new pet-sweep = img src="pet-sweep.jpg";
+var new scissors = img src="img/scissors.jpg";
+var new shark = img src="img/shark.jpg", img src="img/sweep.jpg", img src="img/sweep.jpg", img src="img/tauntaun.jpg", img src="img/unicorn.jpg", img src="img/usb.jeg", img src="img/water-can.jpg"];
+
+
+function firstPic() {
+
+};
+function secondPic(){
+
+};
+
+function thirdPic(){
+
+};
 
 var getNewImage = (){
   for (var i = 0; i === 25; i++) {
@@ -30,3 +73,36 @@ var getNewImage = (){
     }
   };
 };
+//var firstUp = document.getElementById('firstPic');
+//append child
+
+randomTwo = Math.floor(Math.random() *productArray.length);
+while ( randomOne === randomTwo || nextUp.includes(nextUp));
+
+while (randomTwo === rand) {
+
+}
+ var oneClick = document.getElementById('firstPic');
+ oneClick,addEventListener('click', clickCount);
+
+
+
+
+function clickCount(event) {
+  for (var i = 0; i < product.length; i++) {
+
+    if (product[i].id = event.target.id && clickCount < maxClicks)
+    product[i].vote++;
+    clickCount++;
+    nextUp();
+  } else if (clickCount === maxClicks) {
+    oneClick.removeEventListener('click', vote);
+    for (var i = 0; i < productArray.length; i++) {
+      productArray[i].name + '.';
+      if(product[0].dasplay > 0)
+      theList.appendChild(list);
+    }
+    break;
+  }
+}
+cilck

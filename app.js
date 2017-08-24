6d80f75878abe4fe14a10604958067363f7c5f33
mrsshell <mrsshell@comcast.net>
2017-08-24 14:33:18 -0700

    put on the event listener and chart- the list is there totally, but i have it commented out

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function Item (itemName, filePath, timesShown, timesClicked, id) {
   allItems.push(this);
 }
 var allItems = [];
+var countedClicks = [];
 
 var itemName = ['bag', 'boots', 'wine-glass', 'banana', 'bathroom', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can'];
 
@@ -24,7 +25,7 @@ function createItems() {
     new Item(itemName[i], path[i], ids[i]);
   }
 }
-createItem();
+createItems();
 
 var lastRound = [];
 var thisRound = [];
@@ -45,12 +46,12 @@ function makeThreeImages(){
   lastRound = thisRound;
   thisRound = [];
 }
-makeThreeImages();
 
-for (var i = 0; i < document.getElelementsByClassName('clickable').length; i++) {
+for (var i = 0; i < document.getElementsByClassName('clickable').length; i++) {
   var image = document.getElementById('image-' + (i + 1));
   image.addEventListener('click', onClick);
 }
+makeThreeImages();
 
 function onClick (event) {
   var itemIdx = parseInt(event.target.getAttribute('itemIdx'));
@@ -60,72 +61,60 @@ function onClick (event) {
   totalClicks++;
 
   if (totalClicks === maxClicks) {
-    for (var i = 0; i < document.getElelementsByClassName['clikcable'].length; i++){
-      var image = document.getElementById('image' + (i + 1));
+    for (var i = 0; i < document.getElementsByClassName('clickable').length; i++){
+      var image = document.getElementById('image-' + (i + 1));
       image.removeEventListener('click', onClick);
     }
-    var list = document.getElementById('list');
-    for(var j = 0; j < allItems.length; j++){
-      var li = document.createElement('li');
-      li.innerText = allItems[j].itemName = 'was clicked' + allItems[j].timesClicked + 'times';
-      list.appendChild('li');
+    for (var i = 0; i < allItems.length; i++) {
+      countedClicks.push(allItems[i].timesClicked);
     }
+    getChart();
+
+    // var list = document.getElementById('list');
+    // for(var j = 0; j < allItems.length; j++){
+    //   var li = document.createElement('li');
+    //   li.innerText = allItems[j].itemName + ' ' + 'was clicked' + allItems[j].timesClicked + ' ' + 'times';
+    //   list.appendChild(li);
+    // }
   }
 }
-//going to build a chart here
-function something  () {
-  var  ctx = document.getElementById('canvas'.getContext('2d');
-  var chart = new Chart (ctx
 
-}
-
-//this is kind of the properties it should have
-options:
-title: has commas
-legend:
-scales:
-axis
-
-
-//this is the event handler how many clicks - and then set up three new pick- but I THINK i already did this
-function handleClick(event) {
-  if (totalClicks < maxClicks){
-    event.preventDefault();
-    totalClicks++:
-    for (var i = 0; i < allItems.length; i++) {
-      if (allItems[i].name === event.target.id) {
-        allItems[i].timesClicked++;
+function getChart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['bag', 'boots', 'wine-glass', 'banana', 'bathroom', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can'],
+      datasets: [{
+        label: '# of Votes',
+        data: countedClicks,
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 206, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(153, 102, 255)',
+          'rgb(255, 159, 64)'
+        ],
+        borderColor: [
+          'rgba(255,99,132)',
+          'rgba(54, 162, 235)',
+          'rgba(255, 206, 86)',
+          'rgba(75, 192, 192)',
+          'rgba(153, 102, 255)',
+          'rgba(255, 159, 64)'
+        ],
+        borderWidth: 2
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
       }
     }
-    removeOld();
-    makeThreeImages();
-
-  }
-  else {
-    removeOld();
-
-  }
+  });
 }
-//store things in a string array - parseInt and then access through with stringify .
-//store the click array and and display arrray in one object
-
-//localStorage.setItem(JSON.stringify('click', 'display'))
-
-
-//object.timesShown
-//array = getItem(JSON.parse(timesShown))
-//array[i]+= new timesShown
-//send back to localStorage
-
-
-//tracking
-//[timesShown, timesClicked]  var storedTimesShown = [] var storedTimesClicked = []
-
-//if (storage is empty)
-//stored timesShown = timesShown
-//store it();
-//else
-//stores timesShown + timesShown
-//store it();
-
-var clickData =
